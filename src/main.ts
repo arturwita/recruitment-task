ecff6a9f6fa5dd6595fb6846cbe09155f0df8fa6
Artur Wita <witart1998@gmail.com>
2020-10-11 21:59:49 +0200

    fastify replaced by express due to not working middleware

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-    FastifyAdapter,
-    NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestFastifyApplication>(
-        AppModule,
-        new FastifyAdapter({ logger: true })
-    );
-
+    const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe({transform: true}));
 
     const configService: ConfigService = new ConfigService();
