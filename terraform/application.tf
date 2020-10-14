8509fa5a33e2ccfa13ac8a9065c29f06ec40774e
Artur Wita <witart1998@gmail.com>
2020-10-14 10:38:43 +0200

    basic terraform added

---
+++ terraform/application.tf
@@ -0,0 +1,4 @@
+resource "heroku_app" "recruitment_task" {
+  name   = "recruitment-task-86"
+  region = "eu"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "email" {
+  type        = string
+  default     = ""
+  description = "Your email used during Heroku sign-up"
+}
+
+variable "api_key" {
+  type        = string
+  default     = ""
+  description = "Your Heroku API key"
+}
